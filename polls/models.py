6e7a94147aafc0cf38e5b063dbb3962700c02970
sebastianpergo <sebastianperdomopg@gmail.com>
2022-07-15 22:51:24 -0500

    # Adding more data to teh DB

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,27 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    # Method
+    def __str__(self):
+        return self.question_text
+    
+    # timedelta defines the difference between the current time and the pub_date
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Choice(models.Model):
     # Cuando eliminar una pregunta, se eliminan todas las opciones de esa pregunta
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    # Mehod
+    def __str__(self):
+        return self.choice_text
